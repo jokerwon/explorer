c8c51ccd5b711a4ab8fe8015ed80cfa40c1f1b2f
JokerWon <1595672103@qq.com>
2023-07-13 09:12:22 +0800

    feat: add file icon for js, json, txt

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,6 +1,9 @@
 export const DefaultFile = () => <div className="i-vscode-icons:default-file" />
 export const Directory = () => <div className="i-vscode-icons:default-folder" />
 export const Typescript = () => <div className="i-vscode-icons:file-type-typescript" />
+export const Javascript = () => <div className="i-vscode-icons:file-type-js-official" />
+export const Json = () => <div className="i-vscode-icons:file-type-json" />
+export const Text = () => <div className="i-vscode-icons:file-type-text" />
 export const NewFile = () => <div className="i-carbon:document-add" />
 export const NewFolder = () => <div className="i-carbon:folder-add" />
 export const Delete = () => <div className="i-carbon:trash-can" />
@@ -11,6 +14,9 @@ export const DownArrow = () => <div className="i-carbon:chevron-down" />
 
 const fileIconMap: Record<string, any> = {
   ts: <Typescript />,
+  js: <Javascript />,
+  json: <Json />,
+  txt: <Text />,
 }
 
 export const getFileIcon = (type?: string | null) => {
